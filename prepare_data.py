c6f8ed3baeb1cdcf5a88fe90b48f439821e43d95
tu <bhtu.work>
2025-01-18 02:38:49 +0000

    add prepare stacked data

---
+++ prepare_data.py
@@ -0,0 +1,114 @@
+from pathlib import Path
+from typing import Optional
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from data import parquet
+
+
+def listTurbines():
+    dfData = parquet.read("./data/Carrickallen.parquet")
+    return dfData["turbineid"].unique().tolist()
+
+
+def readTurbine(name: Optional[str] = None) -> pd.DataFrame:
+    dfData = parquet.read("./data/Carrickallen.parquet")
+    dfData = dfData[
+        [
+            col
+            for col in dfData.columns
+            if col
+            not in [
+                "capacity",
+                "cutinwindspeed",
+                "dateinstalled",
+            ]
+        ]
+    ]
+
+    if name:
+        dfTurbine = dfData[dfData["turbineid"] == name]
+    dfTurbine.loc[dfTurbine["avgpower"] < 10, "avgpower"] = np.nan
+
+    return dfTurbine
+
+
+def getColumns():
+    return readTurbine().columns
+
+
+def readStackedTurbineData(
+    name: str,
+    n_days=5,
+    shift=1,
+    recompute=False,
+) -> tuple[h5py.Dataset, h5py.File]:
+
+    dataFile = Path("tmp") / f"dataset_{name}_{n_days}_{shift}.hdf5"
+    if recompute:
+        if dataFile.exists():
+            dataFile.unlink()
+
+    if dataFile.exists():
+        f = h5py.File(dataFile, "r+")
+        if name in f:
+            dset = f[name]
+            if isinstance(dset, h5py.Dataset):
+                return dset, f
+            else:
+                raise KeyError(f"Dataset {name} exists but is not a h5py.Dataset")
+        else:
+            f.close()
+
+    dfData = readTurbine(name)
+
+    # fill missing index
+    dfData = dfData.set_index("datetime")
+    fullDateTimeRange = pd.date_range(
+        start=dfData.index.min(), end=dfData.index.max(), freq="10min"
+    )
+    dfFilled = dfData.reindex(fullDateTimeRange)
+    dfFilled = dfFilled.reset_index()
+    dfFilled = dfFilled.rename(columns={"index": "datetime"})
+
+    # stack data
+    N_ROWS_PER_DAY = int(24 * 60 / 10)  # 10 minutes per row
+
+    timeSteps = n_days * N_ROWS_PER_DAY
+
+    dfReduced = dfFilled.drop(
+        columns=[  # drop columns that are for sure not needed
+            "turbineid",
+            "datetime",
+            "site",
+            "phase",
+            "manufacturer",
+            "model",
+        ]
+    )
+
+    arrayReduced = dfReduced.to_numpy()
+    arrayReduced = arrayReduced.astype(np.float32)
+
+    n_stack = (len(arrayReduced) - timeSteps) // shift
+
+    f = h5py.File(dataFile, "a")
+    dset = f.create_dataset(
+        name,
+        (n_stack, timeSteps, arrayReduced.shape[1]),
+        dtype=np.float32,
+    )
+    for i in range(n_stack):
+        dset[i] = arrayReduced[i * shift : i * shift + timeSteps]
+
+    return dset, f
+
+
+if __name__ == "__main__":
+    for turbine in listTurbines():
+        print(f"Reading {turbine}")
+        dset, f = readStackedTurbineData(turbine)
+        print(dset.shape)
+        f.close()

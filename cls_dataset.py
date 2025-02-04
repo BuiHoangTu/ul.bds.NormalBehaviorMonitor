0143dcc300609bc4e0b18372414942846ecd110d
tu <bhtu.work>
2025-02-04 01:27:58 +0000

    include masking to t-dataset

--- cls_dataset.py
+++ cls_dataset.py
@@ -1,12 +1,21 @@
+import numpy as np
 from torch.utils.data import Dataset
 
 
 class TurbineDataset(Dataset):
-    def __init__(self, turbineData3d, rowIndices, featIndices, transform=None):
+    def __init__(
+        self,
+        turbineData3d,
+        rowIndices,
+        featIndices,
+        transform=None,
+        validColId=None,
+    ):
         self.turbineData3d = turbineData3d
         self.rowIndices = rowIndices
         self.featIndices = featIndices
         self.transform = transform
+        self.validColId = validColId
 
     def __len__(self):
         return len(self.rowIndices)
@@ -18,14 +27,30 @@ class TurbineDataset(Dataset):
         if self.transform:
             item = self.transform(item)
 
+        if self.validColId is not None:
+            # 1 for valid, 0 for invalid
+            mask = self.turbineData3d[rowIdx][:, self.validColId].float()
+        else:
+            mask = np.ones(item.shape[0])
+
         # reshape to match the expected input shape of the model
         # add dimension of channel (1 channel)
-        shape = (1,) + item.shape
+        itemShape = (1,) + item.shape
+        item = item.reshape(itemShape)
 
-        return item.reshape(shape)
+        maskShape = (1,) + mask.shape
+        mask = mask.reshape(maskShape)
+
+        return item, mask
 
 
-def trainTestTurbineDataset(turbineData3d, trainIndices, testIndices, featIndices, transform=None):
+def trainTestTurbineDataset(
+    turbineData3d,
+    trainIndices,
+    testIndices,
+    featIndices,
+    transform=None,
+):
     trainDataset = TurbineDataset(turbineData3d, trainIndices, featIndices, transform)
     testDataset = TurbineDataset(turbineData3d, testIndices, featIndices, transform)
 

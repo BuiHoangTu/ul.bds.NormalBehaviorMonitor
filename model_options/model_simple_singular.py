99ba42c4fd4c9f6cfe326d2d528969efaf9383f2
tu <bhtu.work>
2025-04-05 14:32:29 +0100

    move model wrapper to the model file

--- model_options/model_simple_singular.py
+++ model_options/model_simple_singular.py
@@ -1,6 +1,21 @@
 import torch.nn as nn
 
 
+class Autoencoder(nn.Module):
+    def __init__(self, sampleShape, latent_dim: int):
+        super().__init__()
+
+        n_feat, _ = sampleShape
+
+        self.encoder = SimpleEncoder(n_feat, latent_dim)
+        self.decoder = SimpleDecoder(sampleShape)
+
+    def forward(self, x):
+        latent = self.encoder(x)
+        reconstructed = self.decoder(latent)
+        return reconstructed
+
+
 class SimpleEncoder(nn.Module):
     def __init__(
         self,
